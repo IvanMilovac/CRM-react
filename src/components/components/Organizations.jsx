1000f51d08adb51ed24ce14c023a613a9442da87
IvanMilovac <Ivanmilovac1993@hotmail.com>
2022-01-13 22:29:34 +0100

    basic table added

--- src/components/components/Organizations.jsx
+++ src/components/components/Organizations.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
-import Modal from "../shared/Modal";
+import React, { useState, useEffect, useMemo } from "react";
+import { useTable } from "react-table";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import AddOrganizationForm from "./AddOrganizationForm";
 import { collection, getDocs } from "firebase/firestore";
+import Modal from "../shared/Modal";
+import AddOrganizationForm from "./AddOrganizationForm";
 import { db } from "../../firebase-config";
 
 import "./Organization.scss";
@@ -23,6 +24,40 @@ const Organizations = () => {
     };
     fetchOrganizations();
   }, [showModal]);
+  console.log(organizationsList);
+  const tableData = useMemo(
+    () =>
+      organizationsList.map((listItem) => ({
+        name: listItem?.name,
+        industry: listItem?.industry,
+        contact: listItem?.contact,
+        status: listItem?.status?.label,
+      })),
+    [organizationsList]
+  );
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Industry",
+        accessor: "industry",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+      {
+        Header: "Contact",
+        accessor: "contact",
+      },
+    ],
+    []
+  );
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data: tableData });
 
   return (
     <section>
@@ -36,9 +71,60 @@ const Organizations = () => {
       >
         <AddOrganizationForm setShowModal={setShowModal} />
       </Modal>
-      {organizationsList.map((organization) => (
-        <p key={organization.id}>{organization.name}</p>
-      ))}
+      <h2>Organizations</h2>
+      <table {...getTableProps()}>
+        <thead>
+          {
+            // Loop over the header rows
+            headerGroups.map((headerGroup) => (
+              // Apply the header row props
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {
+                  // Loop over the headers in each row
+                  headerGroup.headers.map((column) => (
+                    // Apply the header cell props
+                    <th {...column.getHeaderProps()}>
+                      {
+                        // Render the header
+                        column.render("Header")
+                      }
+                    </th>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </thead>
+        {/* Apply the table body props */}
+        <tbody {...getTableBodyProps()}>
+          {
+            // Loop over the table rows
+            rows.map((row) => {
+              // Prepare the row for display
+              prepareRow(row);
+              return (
+                // Apply the row props
+                <tr {...row.getRowProps()}>
+                  {
+                    // Loop over the rows cells
+                    row.cells.map((cell) => {
+                      // Apply the cell props
+                      return (
+                        <td {...cell.getCellProps()}>
+                          {
+                            // Render the cell contents
+                            cell.render("Cell")
+                          }
+                        </td>
+                      );
+                    })
+                  }
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
       <button
         onClick={() => setShowModal(!showModal)}
         className="organization__add-button"
